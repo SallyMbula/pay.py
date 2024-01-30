00e8f820efb33c7c341460c486f1fbc76535f1bc
Sally Mbula <87859986+SallyMbula@users.noreply.github.com>
2024-01-30 12:38:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Get the student's score
+score = int(input("Enter the student's score: "))
+
+# Assign the grade based on the score
+if score >= 90 and score <= 100:
+  print("your grade is A ")
+else:
+  print("NOT GRADED")
